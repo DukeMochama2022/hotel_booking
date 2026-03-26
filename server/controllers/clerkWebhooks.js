3fd2138feb67f91ccfc0363358d9d6e7e4f3d8c1
DukeMochama2022 <dukemochama21@gmail.com>
2026-03-26 12:02:26 +0300

    debugging

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -5,9 +5,9 @@ const clerkWebhooks = async (req, res) => {
   try {
     const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
     const headers = {
-      svix_id: req.headers["svix_id"],
-      "svix-timestamp": req.headers["svix_id-timestamp"],
-      "svix-signature": req.headers["svix_id-signature"],
+      "svix-id": req.headers["svix-id"],
+      "svix-timestamp": req.headers["svix-timestamp"],
+      "svix-signature": req.headers["svix-signature"],
     };
     //verify headers
     await whook.verify(JSON.stringify(req.body), headers);
@@ -17,8 +17,8 @@ const clerkWebhooks = async (req, res) => {
     const { data, type } = req.body;
     const userData = {
       _id: data.id,
-      email: data.email_addresses[0].email_address,
-      username: data.first_name + "" + data.last_name,
+      email: data.email_addresses?.[0]?.email_address || "",
+      username: data.first_name + " " + data.last_name,
       image: data.image_url,
     };
 
@@ -41,10 +41,10 @@ const clerkWebhooks = async (req, res) => {
       default:
         break;
     }
-    res.json({ success: true, message: "Webhook received" });
+    res.status(200).json({ success: true, message: "Webhook received" });
   } catch (error) {
     console.log(error.message);
-    res.json({ success: false, error: error.message });
+    res.status(400).json({ success: false, error: error.message });
   }
 };
 
